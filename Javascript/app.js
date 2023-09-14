4751e73d681939fe4090c17027e83b3db259a8e1
Juan Cruz <juancruzbarles@gmail.com>
2023-09-13 22:27:13 -0300

    modificacion de proyecto

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,140 +1,43 @@
-let nombreUsuario;
-let calificaciones = [];
+const carrito = [];
+let total = 0;
 
-function calcularPromedio(calificaciones) {
-    let total = 0;
-    for (let i = 0; i < calificaciones.length; i++) {
-        total += calificaciones[i];
-    }
-    return total / calificaciones.length;
+function mostrarModal(producto, precio) {
+    const modalTitle = document.querySelector('.modal-title');
+    const modalBody = document.querySelector('.modal-body');
+
+    modalTitle.textContent = 'Producto Agregado';
+    modalBody.textContent = `Has añadido "${producto}" al carrito por $${precio}.`;
+
+    const modal = new bootstrap.Modal(document.getElementById('productModal'));
+    modal.show();
+
+    // Actualiza el carrito y el precio total
+    carrito.push({ producto, precio });
+    total += precio;
+    actualizarCarrito();
 }
 
-function promocionaMateria(promedio) {
-    if (promedio >= 7) {
-        alert("¡Felicitaciones! Usted promocionó la materia.");
-    } else {
-        alert("Lo siento, no ha promocionado la materia.");
-    }
+function actualizarCarrito() {
+    const carritoElement = document.getElementById('carrito');
+    const carritoTotalElement = document.getElementById('carritoTotal');
+
+    // Actualiza la lista de productos en el carrito
+    carritoElement.innerHTML = '';
+    carrito.forEach(item => {
+        const itemElement = document.createElement('li');
+        itemElement.textContent = `${item.producto} - $${item.precio}`;
+        carritoElement.appendChild(itemElement);
+    });
+
+    // Actualiza el precio total
+    carritoTotalElement.textContent = `Total: $${total}`;
 }
 
-function validarNombre(nombre) {
-    const letras = /^[A-Za-z\s]+$/;
-    return letras.test(nombre);
-}
-
-nombreUsuario = prompt("Por favor, ingrese su nombre completo:");
-
-while (!validarNombre(nombreUsuario)) {
-    alert("El nombre no puede contener números ni símbolos.");
-    nombreUsuario = prompt("Por favor, ingrese su nombre completo:");
-}
-
-alert("Bienvenido " + nombreUsuario + "!");
-
-for (let i = 0; i < 5; i++) {
-    let nota = prompt("Ingrese su calificación " + (i + 1) + ", o escriba 'Listo' para terminar:");
-
-    if (nota.toUpperCase() === "LISTO") {
-        break;
-    }
-
-    nota = parseInt(nota);
-
-    while (isNaN(nota) || nota < 1 || nota > 10) {
-        alert("Ingrese una nota válida entre 1 y 10.");
-        nota = parseInt(prompt("Ingrese su calificación " + (i + 1) + ", o escriba 'Listo' para terminar:"));
-    }
-
-    calificaciones.push(nota);
-}
-
-const opcion = parseInt(prompt("Seleccione una opción digitando su número:\n1. Calcular Promedio\n2. Verificar Promoción"));
-
-switch (opcion) {
-    case 1:
-        // Capturar calificaciones y calcular promedio
-        const promedio = calcularPromedio(calificaciones);
-        alert("El promedio de sus calificaciones es: " + promedio.toFixed(2));
-        break;
-    case 2:
-        // Capturar calificaciones, calcular promedio y verificar promoción
-        const promedioVerificacion = calcularPromedio(calificaciones);
-        promocionaMateria(promedioVerificacion);
-        break;
-    default:
-        alert("Opción inválida");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let nota1 = parseFloat(prompt("Ingrese su primera nota:"));
-let nota2 = parseFloat(prompt("Ingrese su segunda nota:"));
-let nota3 = parseFloat(prompt("Ingrese su tercera nota:"));
-
-let notasValidas = true;
-
-
-
-if (isNaN(nota1) || nota1 > 10){
-    alert("Por favor ingrese sus notas correctamente.")
-    notasValidas = false
-}
-if (isNaN(nota2) || nota2 > 10){
-    alert("Por favor ingrese sus notas correctamente.")
-    notasValidas = false
-}
-if (isNaN(nota3) || nota3 > 10){
-    alert("Por favor ingrese sus notas correctamente.")
-    notasValidas = false
-}
-
-if (notasValidas){
-
-    let promedio = (nota1 + nota2 + nota3)/3;
-
-    if (promedio >= 7) {
-        alert ("Felicitaciones " + nombre + " usted promociono la materia.")
-    } else{
-    alert("El alumno " + nombre + " no promocionó la materia")
-    }
-}*/
-
+const botones = document.querySelectorAll('.btn-add-to-cart');
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const producto = boton.getAttribute('data-product');
+        const precio = parseFloat(boton.getAttribute('data-precio'));
+        mostrarModal(producto, precio);
+    });
+});
